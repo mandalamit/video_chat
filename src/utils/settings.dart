6cb76b565924b46e24c6386963fa9c74769ee52c
Mandalamit <25777690+mandalamit@users.noreply.github.com>
2022-04-02 10:25:02 +0530

    video_call

---
+++ src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = 'f297c3c30cc74492bd7c7e4da0d55579';
+const Token =
+    "006f297c3c30cc74492bd7c7e4da0d55579IACrHu7tDrTBiaU4kY2dKhzCqDiZFGjRA+m8driPLLBCP8KkBc8AAAAAEAAJjRWPBxtUYQEAAQAFG1Rh";
